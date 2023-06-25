66bdb167d236faa166156ab40b9867fa6be8f1fc
JonnerPaz <jonnerpazp@gmail.com>
2023-06-25 19:23:27 -0400

    Update html to allow only numbers on inputs

--- arabigToRoman.js
+++ arabigToRoman.js
@@ -26,6 +26,7 @@ const numbers = {
     milesimas: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000],
   },
 };
+// console.log(numbers['romanos']); // return all romanos measure system
 
 const newNumber = [];
 
